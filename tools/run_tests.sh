50bbcf5883b2144a27af41b430438d7e08f88f12
Sarkoxed <ss2003@yandex.ru>
2021-03-10 02:30:20 +0300

    run_tests

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -6,5 +6,5 @@ echo "" > "logs/$1_log.txt"
 
 for f in $(ls tests/); do
 	echo "testcase: $f"
-	./"$1" < "test/cases/$f" | tee -a logs/"$1_log.txt" || true
+	./"$1" < "test/cases/$f" | sudo tee -a logs/"$1_log.txt" || true
 done
